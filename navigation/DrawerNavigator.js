f478db2b9df42139850a4a622edf6faf9669bf1c
Aadii2508 <73647566+Aadii2508@users.noreply.github.com>
2021-10-30 18:21:45 +0530

    Add files via upload

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import StackNavigator from "./StackNavigator";
+
+
+const Drawer = createDrawerNavigator();
+
+export default class DrawerNavigator extends Component {
+  constructor(props) {
+    super(props);
+    };
+  }
+
